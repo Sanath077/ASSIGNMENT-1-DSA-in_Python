29f6525820bfb9bb414b08be57dcf2e8edb86640
sanath shetty <123662474+Sanath077@users.noreply.github.com>
2023-02-02 23:37:20 +0530

    Update PROBLEM1.py

--- PROBLEM1.py
+++ PROBLEM1.py
@@ -11,7 +11,16 @@ def primepartition(m):
          return True
   return False  
             
-
-
-      
-            
+# The function primepartition(m) checks if an integer m can be expressed as the sum of two prime numbers.
+###############################################################################################################################################################
+# 1.If "m" is less than or equal to 1, the function immediately returns False, as 1 is not considered a prime number.
+# ________________________________________________________________________________________________________________________________________________________________
+# 2.The function is_prime(n) is defined to check if a number n is prime.
+#  It does this by iterating over the numbers from 2 to the square root of n + 1. For each number, it checks if n is divisible by that number.
+#  If it is, it returns False, as n is not prime. If it's not divisible by any number, it returns True, as n is prime.
+# ________________________________________________________________________________________________________________________________________________________________________
+# 3. The for loop for i in range(2, int(m/2)+1) checks for all numbers from 2 to half of m. 
+#    For each number i, it checks if i and m-i are both prime using the is_prime function. If they are, the function returns True, as m can be expressed as the sum of two prime numbers.    
+#  _______________________________________________________________________________________________________________________________________________________________________________________        
+# 4. If the for loop finishes without finding a solution, the function returns False, as m cannot be expressed as the sum of two prime numbers.
+# ____________________________________________________________________________________________________________________________________________________________________________
