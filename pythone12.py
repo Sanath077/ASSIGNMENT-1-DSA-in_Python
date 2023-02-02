1c4a1dda0f802572618c891c45bd8cc1174aedac
Sanath <sanath.21ai043@sode-edu.in>
2023-02-02 23:20:17 +0530

    First Problem

---
+++ pythone12.py
@@ -0,0 +1,17 @@
+def primepartition(m):
+  if m<=1:
+    return False
+  def is_prime(n):
+    for i in range(2,int(n**0.5)+1):
+      if n%i==0:
+        return False
+    return True
+  for i in range(2,int(m/2)+1):
+       if is_prime(i) and is_prime(m-i):
+         return True
+  return False  
+            
+
+
+      
+            
---
+++ pythone13.py
@@ -0,0 +1,10 @@
+def matched(s):
+    stack = []
+    for char in s:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if not stack:
+                return False
+            stack.pop()
+    return not stack
